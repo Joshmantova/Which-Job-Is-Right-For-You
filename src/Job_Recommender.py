18d642a4821ad814f20737b9d3d11bebfee95001
Josh Mantovani <josh.mantova@gmail.com>
2021-03-02 09:56:34 -0700

    Added functionality to streamlit app to mimic flask app

--- src/Job_Recommender.py
+++ src/Job_Recommender.py
@@ -8,8 +8,28 @@ from recommender import Recommender
 def urlify(string):
     str_list = string.split()
     search_keywords = '%20'.join(str_list)
-    url = f'https://www.linkedin.com/jobs/search?keywords={search_keyswords}&location=&trk=guest_job_search_jobs-search-bar_search-submit&redirect=false&position=1&pageNum=0'
+    url = f'https://www.linkedin.com/jobs/search?keywords={search_keywords}&location=&trk=guest_job_search_jobs-search-bar_search-submit&redirect=false&position=1&pageNum=0'
     return url
 
 if __name__ == '__main__':
-    pass
+    list_of_skills = [
+        "Python", "R", "Spark", "SPSS", "SQL", "Pandas", "Numpy", "Cloud", "Docker",
+        "Statistic", "Java", "Scala", "Marketing", "SAS", "Stata", "Excel", "Tableau"
+        ]
+    skills_dict = dict()
+    for skill in list_of_skills:
+        skills_dict[skill] = st.checkbox(skill)
+    
+    user_vector = list(skills_dict.values())
+    done = st.button('Recommend Me A Job!')
+    if done:
+        r = Recommender(user_vector)
+        recs = r.recommend()
+        descrip = r.rec_descrip
+        str_recs = ' '.join(recs)
+        st.write(f"Recommended job title: {recs[0]}")
+        st.write(f"Company: {recs[1]}")
+        st.write(f"Location: {recs[2]}")
+        st.write(f"Job Description: {descrip}")
+        link = f"[Try To Find This Job On Linkedin]({urlify(str_recs)})"
+        st.write(link)
